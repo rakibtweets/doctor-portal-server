e9c70a553c4faaf8d5df1e1a5de1c9d01fffe8c3
rakibtweets <rakibhasanroky0@gmail.com>
2021-11-05 03:19:50 +0600

    database connected

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // midleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhwc1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log('~ uri', uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('database connected');
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Doctors Porter server running');
